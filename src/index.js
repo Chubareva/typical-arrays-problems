349d2882e933a3c5b3659288184c6b01187927c6
Chubareva <33682126+Chubareva@users.noreply.github.com>
2020-10-02 12:06:27 +0300

    refactor: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let minVal = 0;
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (minVal > array[i]) {
+                minVal = array[i];
+            }
+        }
+        return minVal;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let maxVal = 0;
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (maxVal < array[i]) {
+                maxVal = array[i];
+            }
+        }
+        return maxVal;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let sum = 0;
+
+    if (!array || array.length == 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        let numbers = array.length;
+
+        let total = sum / numbers;
+        return total;
+    }
 }
